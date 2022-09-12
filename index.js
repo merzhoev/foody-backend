5b3c9ff1ce37a24c5cd17b34b1b18a008c76de82
Arbi Merzhoev <arbimerzhoev@mail.ru>
2022-09-12 21:46:49 +0300

    remove: trash

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import DishModel from './models/Dish.js';
-
 import { IngrtController, DishController, UserController, FavoriteController } from './controllers/index.js';
 import { authValidation } from './validations.js';
 import { handleValidationErrors, checkAuth } from './middlewares/index.js';
@@ -21,24 +19,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.get('/', async (req, res) => {
-  try {
-    const regex = new RegExp(`(\\s+борщ|^борщ)`, 'i');
-    const [dishes, dishCount] = await Promise.all([
-      DishModel.find({ title: regex }).limit(5),
-      DishModel.countDocuments({ title: regex })
-    ]);
-
-    res.json(dishes);
-  } catch (err) {
-    res.json({ message: 'some error occured' });
-  }
-});
-
-app.get('/second', (req, res) => {
-  res.json({ message: 'secondary route' });
-});
 
 app.post('/auth/login', authValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', authValidation, handleValidationErrors, UserController.register);
